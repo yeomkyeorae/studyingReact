73f99d6792ec4666ab3851b35db554aec0583086
yeomkyeorae <duarufp06@naver.com>
2020-07-18 16:08:43 +0900

    apply props

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const MyComponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = props => {
+  return (
+    <div>
+      안녕하세요. 제 이름은 {props.name}입니다.
+      <br />
+      children 값은 {props.children}
+      입니다.
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  name: '짱구'
 };
 
 export default MyComponent;
