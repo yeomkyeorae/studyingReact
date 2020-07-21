d86dc4c76fd391f31dffe96fbfc642e18d8a5f3c
yeomkyeorae <duarufp06@naver.com>
2020-07-21 16:23:48 +0900

    CH 6 | add input & button

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['빨강', '초록', '파랑', '노랑'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: 'koke' },
+    { id: 2, text: 'saul' },
+    { id: 3, text: 'llorente' },
+    { id: 4, text: 'carrasco' }
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = e => setInputText(e.target.value);
+
+  const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+  return (
+    <div>
+      <input value={inputText} onChange={onChange} />
+      <button>추가</button>
+      <ul>{nameList}</ul>
+    </div>
+  );
 };
 
 export default IterationSample;
