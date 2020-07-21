349003f1a1ade2bdccbbad0f07af15965bd44644
yeomkyeorae <duarufp06@naver.com>
2020-07-21 16:42:55 +0900

    CH 6 | delete element by double click

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -11,12 +11,29 @@ const IterationSample = () => {
   const [nextId, setNextId] = useState(5);
 
   const onChange = e => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText('');
+  };
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id);
+    setNames(nextNames);
+  };
 
-  const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+  const nameList = names.map(name => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
   return (
     <div>
       <input value={inputText} onChange={onChange} />
-      <button>추가</button>
+      <button onClick={onClick}>추가</button>
       <ul>{nameList}</ul>
     </div>
   );
