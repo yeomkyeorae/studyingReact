58b8683c4e11d8c4d4373677a5617bbd78952550
yeomkyeorae <duarufp06@naver.com>
2020-07-22 15:54:17 +0900

    CH 07 | add LIfeCycleSample

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 1677215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
   }
 }
 
