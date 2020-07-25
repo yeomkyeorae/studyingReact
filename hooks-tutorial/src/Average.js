bdc095a91a71225f00f4c7c75587182444d4380d
yeomkyeorae <duarufp06@naver.com>
2020-07-25 10:52:37 +0900

    CH 08 | useMemo

---
+++ hooks-tutorial/src/Average.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const getAverage = numbers => {
+  console.log("평균값 계산 중..");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+
+  const onChange = e => {
+    setNumber(e.target.value);
+  };
+  const onInsert = e => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+  };
+
+  return (
+    <div>
+      <input value={number} onChange={onChange} />
+    </div>
+  );
+};
